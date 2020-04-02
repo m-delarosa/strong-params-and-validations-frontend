ec4d92f13d3daf49b72f2c9d84b98ce4365e3af5
github username <github email address>
2020-04-02 12:15:00 -0600

    Add code from lesson 3

--- app.js
+++ app.js
@@ -1,40 +1,67 @@
 console.log("Good morning, Vietnam!!!")
 
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('%cDOM Content Loaded and Parsed!', 'color: magenta')
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("%cDOM Content Loaded and Parsed!", "color: magenta")
 
-    function getExistingUserData() {
-        fetch('http://localhost:3000/users')
-        .then(response => response.json())
-        .then(result => console.log('users', result))
-    }
+  function getExistingUserData() {
+    fetch("http://localhost:3000/users")
+      .then(response => response.json())
+      .then(result => console.log("users", result))
+  }
 
-    getExistingUserData()
+  getExistingUserData()
 
-    const newUserForm = document.getElementById('add-new-user')
+  const newUserForm = document.getElementById("add-new-user")
 
-    newUserForm.addEventListener('submit', event => {
-        event.preventDefault()
-        const formData = new FormData(newUserForm)
-        const name = formData.get('name')
-        const username = formData.get('username')
-        const email = formData.get('email')
-        const password = formData.get('password')
-        fetch('http://localhost:3000/users', {
-            method: "POST",
-            headers: {
-                "Accept": "application/json",
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({
-                user: {
-                    name: name,
-                    username: username,
-                    email: email,
-                    password: password
-                }
-            })
-        })
+  newUserForm.addEventListener("submit", event => {
+    event.preventDefault()
+    const formData = new FormData(newUserForm)
+    const name = formData.get("name")
+    const username = formData.get("username")
+    const email = formData.get("email")
+    const password = formData.get("password")
+
+    fetch("http://localhost:3000/users", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-type": "application/json"
+      },
+      body: JSON.stringify({
+        user: {
+          name: name,
+          username: username,
+          email: email,
+          password: password
+        }
+      })
     })
-
+      .then(response => response.json())
+      .then(console.log)
+  })
+  //   newUserForm.addEventListener("submit", event => {
+  //     event.preventDefault()
+  //     const formData = new FormData(newUserForm)
+  //     const name = formData.get("name")
+  //     const username = formData.get("username")
+  //     const email = formData.get("email")
+  //     const password = formData.get("password")
+  //     fetch("http://localhost:3000/users", {
+  //       method: "POST",
+  //       headers: {
+  //         Accept: "application/json",
+  //         "Content-Type": "application/json"
+  //       },
+  //       body: JSON.stringify({
+  //         user: {
+  //           name: name,
+  //           username: username,
+  //           email: email,
+  //           password: password
+  //         }
+  //       })
+  //     })
+  //       .then(response => response.json())
+  //       .then(console.log)
+  //   })
 })
